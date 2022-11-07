0f2f5d5fe2795c248e058d2f105b77702bcd26e6
kyseps <screamer971@gmail.com>
2022-11-07 16:34:39 +0330

    latest changes

--- src/components/TopRated.jsx
+++ src/components/TopRated.jsx
@@ -6,24 +6,23 @@ import axios from "axios";
 import { apiKey, baseImgURL, baseURL, putImage } from "./config";
 import { Autoplay, Navigation } from "swiper";
 import { NavLink } from "react-router-dom";
-
-export default function SliderMain() {
+export default function TopRated() {
   const [movies, setMovies] = useState([]);
 
-  async function getMovies() {
+  async function getLatestMovies() {
     const { data } = await axios.get(
-      `${baseURL}/movie/popular?api_key=${apiKey}`
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=e53bab22614cda1579762df1ee0a0c4b`
     );
     setMovies(data.results);
   }
+  console.log(movies);
 
   useEffect(() => {
-    getMovies();
+    getLatestMovies();
   }, []);
-
   return (
-    <section className="my-10 px-5 w-5/5 border-y-2">
-      <h2 className="text-2xl text-rose-700 py-10">Popular Movies</h2>
+    <>
+      <h2 className="text-2xl text-rose-700 py-10">TopRated Movies Movies</h2>
       <Swiper
         navigation={false}
         slidesPerView={7}
@@ -44,13 +43,13 @@ export default function SliderMain() {
               <NavLink to={`/movie/${item.id}`}>
                 <div>
                   <img className="max-h-80" src={putImage(item.poster_path)} />
-                  <p className="text-rose-50">{item.title}</p>
+                  <p className="text-rose-50">{item.original_title}</p>
                 </div>
               </NavLink>
             </SwiperSlide>
           );
         })}
       </Swiper>
-    </section>
+    </>
   );
 }
